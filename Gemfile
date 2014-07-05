35c5b0d56456f60d55917750d70be0de6b7a4f8e
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2014-07-05 15:40:08 -0600

    Draws grid

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'green_shoes'
+gem 'activesupport'
--- app.rb
+++ app.rb
@@ -1,14 +1,37 @@
 require 'green_shoes'
-# Shoes.app do
-#   para "Welcome to Shoes"
-# end
+require 'active_support/all'
 
-Shoes.app width: 300, height: 400 do
+class Map
+  delegate :[], :[]=, to: :@map
 
-  fill rgb(0, 0.6, 0.9, 0.1)
-  stroke rgb(0, 0.6, 0.9)
-  strokewidth 0.25
-  100.times do
-    oval left: (-30..self.width).rand, top: (-30..self.height).rand, radius: (25..50).rand
+  def initialize
+    @map = Array.new(10) { Array.new 10 }
+  end
+
+  def width
+    @map.size
+  end
+
+  def height
+    @map[0].size
+  end
+
+end
+
+Shoes.app width: 600, height: 600 do
+  #fill rgb(0, 0.6, 0.9, 0.1)
+  #stroke rgb(0, 0.6, 0.9)
+  #strokewidth 0.25
+
+  map = Map.new
+  xSize = app.width / map.width
+  ySize = app.height / map.height
+
+  # Draw grid
+  (0...map.height).each do |y|
+    line 0, y * ySize, app.width, y * ySize
+  end
+  (0...map.width).each do |x|
+    line x * xSize, 0, x * xSize , app.height
   end
 end
