2b725cc0e9be3bd9534fd5d0ca57b9691cfbbaf4
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2014-07-06 11:09:41 -0600

    Added another map

--- app.rb
+++ app.rb
@@ -186,17 +186,23 @@ end
 
 maps[:from_behind_double_small_entry] =
   Map.new size: 100, start: [10, 50], goal: [50, 50] do |map|
-  # (30..70).each do |x|
-  #   map[x][30].obstacle = true
-  #   map[x][70].obstacle = true
-  #   map[30][x].obstacle = true
-  #   map[70][x].obstacle = true
-  # end
+  (30..70).each do |x|
+    map[x][30].obstacle = true
+    map[x][70].obstacle = true
+    map[30][x].obstacle = true
+    map[70][x].obstacle = true
+  end
+  (48..52).each do |x|
+    map[70][x].obstacle = false
+  end
   (40..60).each do |x|
     map[x][40].obstacle = true
     map[x][60].obstacle = true
     map[60][x].obstacle = true
     map[40][x].obstacle = true
+  end
+  (48..52).each do |x|
+    map[40][x].obstacle = false
   end
 end
 
@@ -238,7 +244,7 @@ Shoes.app width: 1000, height: 1000 do
     end
   end
 
-  map = maps[:no_solution]
+  map = maps[:from_behind_double_small_entry]
   @xSize = width / map.width
   @ySize = height / map.height
 
