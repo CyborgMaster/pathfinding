b0bb793ddec59af336308782cf8242fbcff1f2ad
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2014-07-06 11:46:37 -0600

    More refactoring

--- a_star.rb
+++ a_star.rb
@@ -1,52 +1,58 @@
-class StarQueue
-  delegate :include?, :empty?, to: :@set
+require 'algorithms'
 
-  def initialize
-    @queue = PriorityQueue.new { |x, y| (x <=> y) == -1 } # Min queue
-    @set = Set.new
-  end
+module AStar
+  class StarQueue
+    delegate :include?, :empty?, to: :@set
 
-  def push(node)
-    @queue.push node, node.guess
-    @set.add node
-  end
+    def initialize
+      @queue = Containers::PriorityQueue.new { |x, y| (x <=> y) == -1 } # Min queue
+      @set = Set.new
+    end
 
-  def pop
-    node = @queue.pop
-    @set.delete node
-    node
-  end
-end
+    def push(node)
+      @queue.push node, node.guess
+      @set.add node
+    end
 
-def a_star(map, visited_callback)
-  open_nodes = StarQueue.new
-  closed_nodes = Set.new
-
-  start = map.start
-  goal = map.goal
-  start.distance = 0
-  start.guess = start.distance_to goal
-  open_nodes.push start
-
-  until open_nodes.empty?
-    current = open_nodes.pop
-    visited_callback.call current
-    return goal if current == goal
-    closed_nodes.add current
-    current.neighbors.each do |neighbor|
-      next if neighbor.obstacle
-      next if closed_nodes.include? neighbor
-      distance = current.distance + current.distance_to(neighbor)
-      guess = distance + neighbor.distance_to(goal)
-      if guess < neighbor.guess
-        neighbor.distance = distance
-        neighbor.guess = guess
-        neighbor.from = current
-      end
-      open_nodes.push neighbor unless open_nodes.include? neighbor
+    def pop
+      node = @queue.pop
+      @set.delete node
+      node
     end
   end
 
-  # No path to goal
-  return nil
+  module_function
+
+  def search(map, visited_callback)
+    open_nodes = StarQueue.new
+    closed_nodes = Set.new
+
+    start = map.start
+    goal = map.goal
+    start.distance = 0
+    start.guess = start.distance_to goal
+    open_nodes.push start
+
+    until open_nodes.empty?
+      current = open_nodes.pop
+      visited_callback.call current
+      return goal if current == goal
+      closed_nodes.add current
+      current.neighbors.each do |neighbor|
+        next if neighbor.obstacle
+        next if closed_nodes.include? neighbor
+        distance = current.distance + current.distance_to(neighbor)
+        guess = distance + neighbor.distance_to(goal)
+        if guess < neighbor.guess
+          neighbor.distance = distance
+          neighbor.guess = guess
+          neighbor.from = current
+        end
+        open_nodes.push neighbor unless open_nodes.include? neighbor
+      end
+    end
+
+    # No path to goal
+    return nil
+  end
 end
--- app.rb
+++ app.rb
@@ -1,106 +1,8 @@
 require 'green_shoes'
 require 'active_support/all'
-require 'algorithms'
 
 require_relative 'map'
 require_relative 'a_star'
-
-include Containers
-
-class Node
-  attr_accessor :location, :distance, :guess, :from, :obstacle
-  attr_reader :neighbors
-
-  def initialize(location)
-    @location = location
-    @neighbors = []
-    @guess = Float::INFINITY
-  end
-
-  def distance_to(other)
-    location.distance_to other.location
-  end
-end
-
-Point = Struct.new :x, :y do
-  def distance_to(other)
-    Math.hypot(other.x - x, other.y - y)
-  end
-end
-
-
-maps = {}
-maps[:empty] = Map.new
-maps[:simple] = Map.new do |map|
-  (2..6).each do |x|
-    map[x][2].obstacle = true
-    map[7][x].obstacle = true
-  end
-end
-maps[:empty_large] = Map.new size: 100
-maps[:simple_large] = Map.new size: 100 do |map|
-  (20..60).each do |x|
-    map[70][x].obstacle = true
-  end
-end
-maps[:blocked] = Map.new size: 100 do |map|
-  (20..70).each do |x|
-    map[x][20].obstacle = true
-    map[70][x].obstacle = true
-  end
-end
-maps[:from_behind] = Map.new size: 100, start: [10, 50], goal: [50, 50] do |map|
-  (30..70).each do |x|
-    map[x][30].obstacle = true
-    map[x][70].obstacle = true
-    map[30][x].obstacle = true
-  end
-end
-
-maps[:from_behind_double] = Map.new size: 100, start: [10, 50], goal: [50, 50] do |map|
-  (30..70).each do |x|
-    map[x][30].obstacle = true
-    map[x][70].obstacle = true
-    map[30][x].obstacle = true
-  end
-  (40..60).each do |x|
-    map[x][40].obstacle = true
-    map[x][60].obstacle = true
-    map[60][x].obstacle = true
-  end
-end
-
-maps[:no_solution] =
-  Map.new size: 100, start: [10, 50], goal: [50, 50] do |map|
-  (40..60).each do |x|
-    map[x][40].obstacle = true
-    map[x][60].obstacle = true
-    map[60][x].obstacle = true
-    map[40][x].obstacle = true
-  end
-end
-
-maps[:from_behind_double_small_entry] =
-  Map.new size: 100, start: [10, 50], goal: [50, 50] do |map|
-  (30..70).each do |x|
-    map[x][30].obstacle = true
-    map[x][70].obstacle = true
-    map[30][x].obstacle = true
-    map[70][x].obstacle = true
-  end
-  (48..52).each do |x|
-    map[70][x].obstacle = false
-  end
-  (40..60).each do |x|
-    map[x][40].obstacle = true
-    map[x][60].obstacle = true
-    map[60][x].obstacle = true
-    map[40][x].obstacle = true
-  end
-  (48..52).each do |x|
-    map[40][x].obstacle = false
-  end
-end
 
 Thread::abort_on_exception = true
 
@@ -141,7 +43,7 @@ Shoes.app width: 1000, height: 1000 do
     end
   end
 
-  map = maps[:from_behind_double_small_entry]
+  map = Maps::MAPS[:from_behind_double_small_entry]
   @xSize = width / map.width
   @ySize = height / map.height
 
@@ -156,7 +58,7 @@ Shoes.app width: 1000, height: 1000 do
   end
 
   Thread.new do
-    path = a_star map, visited_callback
+    path = AStar::search map, visited_callback
     draw_path path unless path.nil?
   end
 end
